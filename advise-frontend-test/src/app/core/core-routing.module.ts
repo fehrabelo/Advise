01d121e25e4213bc3269f45ed965e2c4131e5be5
feh_rabelo <felipe.leite@groupsky.com.br>
2021-11-09 02:51:55 -0300

    home structure initialization

--- advise-frontend-test/src/app/core/core-routing.module.ts
+++ advise-frontend-test/src/app/core/core-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeListComponent } from './pages/list/poke-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PokeListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
